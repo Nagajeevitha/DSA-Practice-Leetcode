6cb6be3405c7184684bfd0fe2fbb0922e162633e
Nagajeevitha <119517074+Nagajeevitha@users.noreply.github.com>
2025-06-19 08:51:43 +0530

    Create 155. Min Stack.java
    ## ✅ 155. Min Stack
    
    **Problem Link**: https://leetcode.com/problems/min-stack/
    
    **Question Description**:
    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
    
    Implement the MinStack class:
    
    MinStack() initializes the stack object.
    void push(int val) pushes the element val onto the stack.
    void pop() removes the element on the top of the stack.
    int top() gets the top element of the stack.
    int getMin() retrieves the minimum element in the stack.
    You must implement a solution with O(1) time complexity for each function.
    
     
    
    Example 1:
    
    Input
    ["MinStack","push","push","push","getMin","pop","top","getMin"]
    [[],[-2],[0],[-3],[],[],[],[]]
    
    Output
    [null,null,null,null,-3,null,0,-2]
    
    Explanation
    MinStack minStack = new MinStack();
    minStack.push(-2);
    minStack.push(0);
    minStack.push(-3);
    minStack.getMin(); // return -3
    minStack.pop();
    minStack.top();    // return 0
    minStack.getMin(); // return -2
     
    
    Constraints:
    
    -231 <= val <= 231 - 1
    Methods pop, top and getMin operations will always be called on non-empty stacks.
    At most 3 * 104 calls will be made to push, pop, top, and getMin.

---
+++ 155. Min Stack.java
@@ -0,0 +1,37 @@
+class MinStack {
+    Stack<Integer> s;
+    public MinStack() {
+        s = new Stack<>();
+    }
+    
+    public void push(int val) {
+        s.push(val);
+    }
+    
+    public void pop() {
+        s.pop();
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        int min = Integer.MAX_VALUE;
+
+        for(int e:s){
+            if(min>e)
+                min=e;
+        }
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
